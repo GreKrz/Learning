ae6bff2f0e8b4004e173d7fbcb681142bf97e889
ziomalzgor <ziomalzgor@192.168.0.10>
2018-09-14 17:27:23 +0200

    tutorial2 Data types

---
+++ Learning/src/tutorial2/App2.java
@@ -0,0 +1,34 @@
+package tutorial2;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		int myNumber = 88;
+		
+		short myShort = 847;
+		long myLong = 9797;
+		
+		double myDouble = 7.3433;
+		
+		float myFloat = 3.14f;
+		
+		char myChar1 = 'y';
+		char myChar2 = '#';
+		
+		boolean myBoolean = true;
+		
+		byte myByte = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar1);
+		System.out.println(myChar2);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+
+	}
+
+}
