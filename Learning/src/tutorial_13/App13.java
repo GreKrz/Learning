00c6e1bd0f9336af8118f669a3f4ff0d9bdb3c68
ziomalzgor <ziomalzgor@192.168.0.10>
2018-10-26 17:15:12 +0200

    tut 13 update

--- Learning/src/tutorial_13/App13.java
+++ Learning/src/tutorial_13/App13.java
@@ -11,7 +11,17 @@ class Person {
 	
 	// 1. Data
 	// 2. Subroutines (methods)
+	void speak() {
+		
+		for(int i= 0; i<3; i++) {
+		System.out.println("My name is: " + name + " and I am " + age + " years old.");
+		}
+	}
 	
+	void sayHello() {
+		
+		System.out.println("Hello there!");
+	}
 	
 }
 
@@ -31,29 +41,35 @@ public class App13 {
 		
 		person1.name = "Joe Bloggs";
 		person1.age = 37;
+		person1.speak();
+		person1.sayHello();
 		
 		Person person2 = new Person();
 		person2.name = "Sarah Smith";
 		person2.age = 20;
+		person2.speak();
+		person2.sayHello();
 		
 		System.out.println(person1.name);
 	
-		Person person3 = new Person();
-		person3.name = "Marcin S.";
-		person3.age = 28;
+//		Person person3 = new Person();
+//		person3.name = "Marcin S.";
+//		person3.age = 28;
+//		
+//		Requirements wymagania = new Requirements();
+//		wymagania.data = "wiecej wymagan";
+//		wymagania.dataNumbers = 2;
+//		
+//		if(person3.name == "Marcin S.") {
+//			for( int i=3;i>2;i++) {
+//			wymagania.dataNumbers++;
+//			System.out.println(wymagania.data);
+//			System.out.println(wymagania.dataNumbers);
+//			}
+//		}
+//		System.out.println(wymagania.dataNumbers);
+//		
 		
-		Requirements wymagania = new Requirements();
-		wymagania.data = "wiecej wymagan";
-		wymagania.dataNumbers = 2;
-		
-		if(person3.name == "Marcin S.") {
-			for( int i=3;i>2;i++) {
-			wymagania.dataNumbers++;
-			System.out.println(wymagania.data);
-			System.out.println(wymagania.dataNumbers);
-			}
-		}
-		System.out.println(wymagania.dataNumbers);
 		
 	}
 
